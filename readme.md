de5f160cd5d8208c51ee576145a050865ce5ef16
Mihai Blaga <legolas8911@gmail.com>
2020-04-21 00:17:57 +0200

    added cron support and limit (minor)

--- readme.md
+++ readme.md
@@ -1,8 +1,13 @@
-Checks AH delivery slots by postcode, in this time of need, without having to access AH's website and scroll the calendar.
+## Albert Heijn Delivery Check
+
+Checks Albert Heijn delivery slots by postcode, in this time of need, without having to access AH's website and scroll the calendar.
 
 `npm install -g @mihaiblaga89/ah-delivery` 
 `ahdl check <postcode>`
 
 or `npx @mihaiblaga89/ah-delivery check <postcode>`
 
-Will add cronjob support later
+### Options
+`-c, --cron` - Will exit with an exit code of 1 (error) if it finds something. Made specifically for my Synology NAS since I can easily set it up with a few clicks to send me an email if a cronjob fails. That way I can enable this to run every 10 minutes and I'll receive an email if it finds a slot. 
+
+`-l, --limit <days>` - You can limit the number of days you want this script to check. Maybe you're not interested in slots 10 days from now. 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import { program } from 'commander';
 import { table } from 'table';
+import moment from 'moment';
 
 const execute = async postcode => {
   const { data } = await axios.get(`https://www.ah.nl/service/rest/delegate?url=%2Fkies-moment%2Fbezorgen%2F${postcode}&_=${Date.now()}`, {
@@ -10,12 +11,22 @@ const execute = async postcode => {
     },
   });
 
-  const dates = data._embedded.lanes[3]._embedded.items[0]._embedded.deliveryDates;
+  let dates = data._embedded.lanes[3]._embedded.items[0]._embedded.deliveryDates;
+  const { cron, limit } = program;
+
+  // filter the results if we have a limit
+  if (limit) {
+    const daysLimit = parseInt(program.limit, 10);
+    if (Number.isNaN(daysLimit) || daysLimit <= 0) throw new Error('Limit must be a number (of days) and it must be > 0');
+    const limitDate = moment().add(daysLimit, 'days');
+    dates = dates.filter(date => moment(date.date, 'YYYY-MM-DD').isBefore(limitDate));
+  }
 
   const availableDatesAndSlots = [];
 
   dates.forEach(date => {
     if (!date.deliveryTimeSlots.length) return false;
+
     const availableSlots = date.deliveryTimeSlots.filter(slot => slot.state !== 'full');
     if (!availableSlots.length) return false;
 
@@ -23,17 +34,37 @@ const execute = async postcode => {
       availableDatesAndSlots.push({ date: date.date, slot: `From ${slot.from} to ${slot.to}` });
     });
   });
+
   let tableOutput = [];
+
   if (availableDatesAndSlots.length) {
     tableOutput = availableDatesAndSlots.map(available => [available.date, available.slot]);
+
     tableOutput.unshift(['Available dates', ['Available slots']]);
   } else {
-    tableOutput.push([`No available slots between ${dates[0].date} and ${dates[dates.length - 1].date} :( `]);
+    tableOutput.push([
+      `No available slots between ${moment().format('YYYY-MM-DD')} and ${
+        limit
+          ? moment()
+              .add(limit, 'days')
+              .format('YYYY-MM-DD')
+          : dates[dates.length - 1].date
+      } :( `,
+    ]);
   }
+
   console.log(table(tableOutput));
+
+  // exit with error on cron flag
+  if (cron && availableDatesAndSlots.length) process.exit(1);
 };
 
 program
+  .option(
+    '-c, --cron',
+    "made especially for Synology. You can easily setup Synology to send an email if a cron terminates unexpectedly. The script will terminate with an error if it finds something or with success if it doesn't"
+  )
+  .option('-l, --limit <number>', 'limits results. Ex. -l 5 will return only results for the next 5 days and discard the rest')
   .command('check <postcode>')
   .description('Check AH delivery')
   .action(postcode => {
